f12d4c7eff80410273affb05d54755e3c6e3d03f
hascr <harold.araya@gmail.com>
2024-03-04 17:42:30 -0600

    prueba 2

--- has_labs/__manifest__.py
+++ has_labs/__manifest__.py
@@ -4,7 +4,7 @@
 
     #'summary': """
     #    Short (1 phrase/line) summary of the module's purpose, used as
-    #    subtitle on modules listing or odoo.com""",
+    #    subtitle on modules listing or odoo""",
 
     #'description': """
     #    Long description of module's purpose
