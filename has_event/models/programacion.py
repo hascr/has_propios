a6f78365b7d8c365e5ba481c81493f9f1ba6d08b
hascr <harold.araya@gmail.com>
2024-04-24 07:55:33 -0600

    módulo de programación

--- has_event/models/programacion.py
+++ has_event/models/programacion.py
@@ -10,9 +10,19 @@ _logger = logging.getLogger(__name__)
 class Programacion(models.Model):
     _name = 'programacion'
     _description = _('Programacion de sesiones de entrenamiento')
+    _rec_name = 'cod_nombre'
 
     codigo = fields.Char(string="Código")
     curso = fields.Char(string="Curso")
     cuenta = fields.Char(string="Cuenta")
     inicio = fields.Datetime(string="Inicio")
     fin = fields.Datetime(string="Fin")
+    u_clase = fields.Boolean(string="Última clase")
+
+    cod_nombre = fields.Char(string='Curso', compute='_computeVar')
+
+
+    @api.depends('codigo', 'curso')
+    def _computeVar(self):
+        for line in self:
+            line.cod_nombre = f"{line.codigo} - {line.curso}"
