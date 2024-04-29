38ed268ccf7650bbd05800cb65d0549c8dd1093e
hascr <harold.araya@gmail.com>
2024-04-29 11:08:43 -0600

    corrección de envío automático

--- payment_report_send_by_mail/models/payment_report_send.py
+++ payment_report_send_by_mail/models/payment_report_send.py
@@ -18,7 +18,8 @@ class account_payment(models.Model):
     def action_post(self):
         res = super(account_payment, self).action_post()
         if self.partner_type == 'supplier' and self.partner_id.parent_id.custom_allow_send_mail or self.partner_id.custom_allow_send_mail:
-            template = self.env.ref('mail_template_data_payment_receipt_supplier', False)
+            template = self.env.ref('payment_report_send_by_mail.mail_template_data_payment_receipt_supplier', False)
+            print(template)
             template.send_mail(self.id)
         return res
     
