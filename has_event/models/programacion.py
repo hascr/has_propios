4141e46cb61a96c203fecaa5dfcf49aa2d5fa3ad
hascr <harold.araya@gmail.com>
2024-05-17 13:37:59 -0600

    mejoras programacion

--- has_event/models/programacion.py
+++ has_event/models/programacion.py
@@ -20,9 +20,35 @@ class Programacion(models.Model):
     u_clase = fields.Boolean(string="Última clase")
 
     cod_nombre = fields.Char(string='N. Curso', compute='_computeVar')
-
+    instructor = fields.Char(string='Instructor', compute='_compute_instructor')
+    matricula = fields.Char(string='URL matrícula', compute='_compute_urlmatricula')
 
     @api.depends('codigo', 'curso')
     def _computeVar(self):
         for line in self:
             line.cod_nombre = f"{line.codigo} - {line.curso}"
+
+    @api.depends('codigo')
+    def _compute_instructor(self):
+        for record in self:
+            # Search for cursos records with matching codigo in the id field
+            curso_record = self.env['cursos'].search([('id', '=', record.codigo)])
+            if curso_record:
+                record.instructor = curso_record.instructor  # Update instructor
+            else:
+                record.instructor = ''  # Set empty string if no match
+
+    @api.depends('codigo')
+    def _compute_urlmatricula(self):
+        for record in self:
+            # Search for cursos records with matching codigo in the id field
+            curso_record = self.env['cursos'].search([('id', '=', record.codigo)])
+            if curso_record:
+                record.matricula = curso_record.matricula  # Update matricula
+            else:
+                record.matricula = ''  # Set empty string if no match
+
+
+    def write(self, vals):
+        res = super(Programacion, self).write(vals)
+        return res
