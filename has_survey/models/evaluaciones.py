07e45c5a36b9a7a2173c10a6d733a4350b7db247
hascr <harold.araya@gmail.com>
2024-05-23 11:18:49 -0600

    actualización evaluaciones

--- has_survey/models/evaluaciones.py
+++ has_survey/models/evaluaciones.py
@@ -7,8 +7,9 @@ from odoo.exceptions import UserError, ValidationError
 _logger = logging.getLogger(__name__)
 
 
-class Evaluaciones(models.Model):
+class evaluaciones(models.Model):
     _name = 'evaluaciones'
+    _auto = False
     _description = _('Evaluaciones')
 
     name = fields.Char(_('Participante'))
@@ -18,46 +19,38 @@ class Evaluaciones(models.Model):
     curso = fields.Char(_('Curso'))
     asesor = fields.Char(_('Asesor'))
     intereses = fields.Char(_('Intereses'))
-    correo_interes = fields.Char(_('Correo Interés'))
+    correo_intereses = fields.Char(_('Correo Interés'))
     empresa = fields.Char(_('Empresa'))
     whatsapp = fields.Char(_('WhatsApp'))
-    Comentarios = fields.Text(_('Comentarios'))
+    comentarios = fields.Text(_('Comentarios'))
     correo = fields.Char(_('Correo Contacto'))
     telefono = fields.Char(_('Teléfono'))
     instructor = fields.Char(_('Instructor'))
     duracion = fields.Float(_('Duración'))
 
-
-def init(self):
+    def init(self):
         self._cr.execute("""
-            CREATE OR REPLACE VIEW estudiantes AS (
-            SELECT 
- 		e.id AS id,
-		e.name AS nombre,
-		concat(date_part('year'::text, e.create_date), to_char(e.create_date, 'MM'::text), '-', e.id) AS codest,
-    	btrim((e.email)::text) AS correo,
-    	t.id AS codigo,
-    	t.name->> 'es_CR' AS curso,
-	   t.urlmatricula AS matricula,
-   	(SELECT ta.cuenta FROM training_account ta WHERE ta.id = t.cuenta_id) AS cuenta,
-    	t.date_begin AS inicio,
-    	to_char(t.date_begin, 'yyyy-mm-dd'::text) AS inicio_corto,
-    	t.date_end AS final,
-    	e.nota AS nota,
-    	((t.cantsesion)::numeric * (t.hsesion)::numeric) AS tiempototal,
-    	e.asistencia AS horas,
-    	(SELECT rp1.email FROM res_partner rp1 JOIN res_users ru1 on ru1.partner_id = rp1.id WHERE ru1.id = e.asesor) AS asesor,
-    	e.id AS estudiante,
-    	t.presencial AS presencial,
-    	t.noenviar AS noenviar,
-    	e.licencia AS licencia,
-    	t.enofi AS lugar,
-    	t.fechas_teams_pres AS fechas,
-    	btrim((e.name)::text) AS nombres,
-		concat('(',e.id,')') AS apellidos 
+            CREATE OR REPLACE VIEW evaluaciones AS (
+            SELECT		s.id AS id,
+				s.end_datetime AS fecha_evaluacion,
+				s.scoring_percentage AS nota,
+				(SELECT a."value"->> 'es_CR' FROM survey_question_answer a WHERE a.id = (SELECT MAX(l.suggested_answer_id) filter (WHERE l.question_id = 41) FROM	survey_user_input_line l WHERE l.user_input_id = s.id)) AS enviar_info,
+				(SELECT a."value"->> 'es_CR' FROM survey_question_answer a WHERE a.id = (SELECT MAX(l.suggested_answer_id) filter (WHERE l.question_id = 27) FROM	survey_user_input_line l WHERE l.user_input_id = s.id)) AS curso,
+				(SELECT a."value"->> 'es_CR' FROM survey_question_answer a WHERE a.id = (SELECT MAX(l.suggested_answer_id) filter (WHERE l.question_id = 28) FROM	survey_user_input_line l WHERE l.user_input_id = s.id)) AS asesor,
+				(SELECT string_agg(SUBSTRING(a."value"->> 'es_CR', 1, LENGTH(a."value"->> 'es_CR') -0), ', ') AS answer_texts FROM survey_question_answer a WHERE a.id IN ( SELECT suggested_answer_id    FROM survey_user_input_line l    WHERE l.question_id = 45 AND l.user_input_id = s.id)) AS intereses,
+				(SELECT MAX(l.value_ans_sh_email) filter (WHERE l.question_id = 42) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS correo_intereses,
+				(SELECT MAX(l.value_char_box) filter (WHERE l.question_id = 47) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS name,
+				(SELECT MAX(l.value_char_box) filter (WHERE l.question_id = 48) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS empresa,
+				CONCAT((SELECT MAX(l.value_char_box) filter (WHERE l.question_id = 43) FROM	survey_user_input_line l WHERE l.user_input_id = s.id),'-',(SELECT MAX(l.value_char_box) filter (WHERE l.question_id = 44) FROM	survey_user_input_line l WHERE l.user_input_id = s.id)) AS WhatsApp,
+				(SELECT MAX(l.value_text_box) filter (WHERE l.question_id = 46) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS comentarios,
+				(SELECT MAX(l.value_ans_sh_email) filter (WHERE l.question_id = 49) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS correo,
+				(SELECT MAX(l.value_char_box) filter (WHERE l.question_id = 50) FROM	survey_user_input_line l WHERE l.user_input_id = s.id) AS telefono,
+				(SELECT c.instructor FROM cursos c WHERE concat(c.codigo,' - ',c.curso) = (SELECT a."value"->> 'es_CR' FROM survey_question_answer a WHERE a.id = (SELECT MAX(l.suggested_answer_id) filter (WHERE l.question_id = 27) FROM	survey_user_input_line l WHERE l.user_input_id = s.id))) AS instructor,
+				(SELECT c.sesiones * c.horassesion FROM cursos c WHERE concat(c.codigo,' - ',c.curso) = (SELECT a."value"->> 'es_CR' FROM survey_question_answer a WHERE a.id = (SELECT MAX(l.suggested_answer_id) filter (WHERE l.question_id = 27) FROM	survey_user_input_line l WHERE l.user_input_id = s.id))) AS duracion
 
-   FROM event_registration e
-   	JOIN event_event t ON t.id = e.event_id
-  	WHERE t.stage_id != 5
-	AND e.state != 'cancel');
+FROM			survey_user_input s
+
+WHERE			s.STATE = 'done'
+AND			s.survey_id = 3
+                         );
             """)
