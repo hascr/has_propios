80df1759d6af2941d11d8d96592804736a51b119
hascr <harold.araya@gmail.com>
2024-06-17 13:48:34 +0000

    agregar area de trabajo a leads

--- has_area_trabajo/models/res_partner.py
+++ has_area_trabajo/models/res_partner.py
@@ -7,3 +7,4 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     area_id = fields.Many2one(comodel_name='area.trabajo', string='Area de trabajo')
+    _sql_constraints = [('email_unique', 'unique(email)', "El correo debe ser único")]
