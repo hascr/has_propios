a35134f2804f1c7c1e976d14d534b410eeb781af
hascr <harold.araya@gmail.com>
2024-06-20 10:29:12 -0600

    actualización

--- has_area_trabajo/models/res_partner.py
+++ has_area_trabajo/models/res_partner.py
@@ -7,4 +7,3 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     area_id = fields.Many2one(comodel_name='area.trabajo', string='Area de trabajo')
-    _sql_constraints = [('email_unique', 'unique(email)', "El correo debe ser único")]
