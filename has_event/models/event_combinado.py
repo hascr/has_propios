01e1f516c7ac7fbd90c8d243b9b5932cd23f1390
hascr <harold.araya@gmail.com>
2024-07-03 08:18:03 -0600

    filtro evaluaciones

--- has_event/models/event_combinado.py
+++ has_event/models/event_combinado.py
@@ -10,21 +10,41 @@ _logger = logging.getLogger(__name__)
 class eventcombinado(models.Model):
     _name = 'event.combinado'
     _auto = False
-    _description = 'Evento combinado'
+    _description = 'Evaluación cursos'
 
     name = fields.Char(
-        string='Nombre combinado'
-    )
-    
-
-
+        string='Nombre combinado')
+ 
     def init(self):
         self._cr.execute("""
             CREATE OR REPLACE VIEW event_combinado AS (
-            SELECT		e.id as id,
-			concat(e.id,' - ',e.name ->> 'es_CR') as name
+            SELECT	    e.id as id,
+			            concat(e.id,' - ',e.name ->> 'es_CR') as name
 
-FROM		event_event e
+            FROM		event_event e
 
-WHERE		e.stage_id = 3);
+            WHERE		e.stage_id = 3
+            AND		e.event_type_id = 1
+                         );
+            """)
+
+class eventwebinars(models.Model):
+    _name = 'event.webinars'
+    _auto = False
+    _description = 'Evaluación webinars'
+
+    name = fields.Char(
+        string='Nombre combinado')
+ 
+    def init(self):
+        self._cr.execute("""
+            CREATE OR REPLACE VIEW event_combinado AS (
+            SELECT	    e.id as id,
+			            concat(e.id,' - ',e.name ->> 'es_CR') as name
+
+            FROM		event_event e
+
+            WHERE		e.stage_id = 3
+            AND		e.event_type_id = 2
+                         );
             """)
