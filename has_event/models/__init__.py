8249bf291b1f1ce34c9ced96e26d0f77004d08d9
hascr <harold.araya@gmail.com>
2024-07-03 09:07:45 -0600

    error en modelo webinars

--- has_event/models/__init__.py
+++ has_event/models/__init__.py
@@ -2,7 +2,6 @@
 
 from . import event_event
 from . import training_account
-#from . import instructor_name
 from . import event_registration
 from . import hr_employee
 from . import res_country 
@@ -11,3 +10,4 @@ from . import cursos
 from . import estudiantes
 from . import event_combinado
 from . import asistencia
+from . import event_webinars
--- has_event/models/event_webinars.py
+++ has_event/models/event_webinars.py
@@ -7,23 +7,23 @@ from odoo.exceptions import UserError, ValidationError
 _logger = logging.getLogger(__name__)
 
 
-class eventcombinado(models.Model):
-    _name = 'event.combinado'
+class eventwebinars(models.Model):
+    _name = 'event.webinars'
     _auto = False
-    _description = 'Evaluación cursos'
+    _description = 'Evaluación webinars'
 
     name = fields.Char(
         string='Nombre combinado')
  
     def init(self):
         self._cr.execute("""
-            CREATE OR REPLACE VIEW event_combinado AS (
+            CREATE OR REPLACE VIEW event_webinars AS (
             SELECT	    e.id as id,
 			            concat(e.id,' - ',e.name ->> 'es_CR') as name
 
             FROM		event_event e
 
             WHERE		e.stage_id = 3
-            AND		e.event_type_id = 1
+            AND		e.event_type_id = 2
                          );
             """)
