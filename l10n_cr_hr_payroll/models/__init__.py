f49cfa271671e0874a300887ea748b222e1c5a4f
hascr <harold.araya@gmail.com>
2024-07-05 11:50:12 -0600

    actualización planillas y campo fecha en cotizaciones

--- l10n_cr_hr_payroll/models/__init__.py
+++ l10n_cr_hr_payroll/models/__init__.py
@@ -3,3 +3,4 @@ from . import hr_contract
 from . import res_company
 from . import res_config_settings
 from . import hr_payslip_run
+from . import hr_leave
---
+++ l10n_cr_hr_payroll/models/hr_leave.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class Hr_leave(models.Model):
+    _inherit = 'hr.leave'
+
+    signature = fields.Binary('Firma colaborador', required=True)
