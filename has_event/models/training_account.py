0d53f909a8d9c5a8b5265e2baeeed8149fcf945f
hascr <harold.araya@gmail.com>
2024-11-20 07:27:53 -0600

    fecha vencimiento cuentas

--- has_event/models/training_account.py
+++ has_event/models/training_account.py
@@ -4,8 +4,10 @@ from odoo import models, fields, api
 
 
 class trainingaccount(models.Model):
-    _name = 'training.account'
-    _rec_name = 'cuenta'
-    _description = 'Cuenta Training'
+    _name = "training.account"
+    _rec_name = "cuenta"
+    _description = "Cuenta Training"
 
-    cuenta = fields.Char(string='Cuenta Training')
+    cuenta = fields.Char(string="Cuenta Training")
+    active = fields.Boolean("Activo", default=True)
+    expiration_date = fields.Date(string="Expira")
