ca3296021fa59eae581b296925a0d188310de523
hascr <harold.araya@gmail.com>
2024-11-26 23:35:50 +0000

    modulo evaluaciones

---
+++ has_evaluaciones/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'HAS - Evaluaciones',
+    #'version': '1.0.0',
+    'description': """ Agregar vista de evaluaciones """,
+    'summary': """ Agregar vista de evaluaciones """,
+    'author': 'HAS',
+    'website': '',
+    'category': '',
+    'depends': ['base','survey'],
+    "data": [
+        "views/virtuales_views.xml",
+        "security/ir.model.access.csv",
+        #"views/presenciales_views.xml",
+        #"views/webinars_views.xml",
+        "views/survey_templates.xml",
+        #"views/survey_user_input_views.xml",
+    ],
+    'application': True,
+    #'installable': True,
+    #'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ has_evaluaciones/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import virtuales
+#from . import presenciales
+#from . import webinars
