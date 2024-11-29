bab802e4c661a02a0c1ffefe9b0223fe81ce7aee
hascr <harold.araya@gmail.com>
2024-11-29 07:08:11 -0600

    Agregar no recordatorio de factura

--- has_invoice_extend/models/account_move.py
+++ has_invoice_extend/models/account_move.py
@@ -3,35 +3,25 @@ from odoo import api, fields, models, tools
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
-    instructor = fields.Boolean(
-        related='partner_id.instructor',
-        string='Instructor'
-    )
+    instructor = fields.Boolean(related="partner_id.instructor", string="Instructor")
     """ aprobada = fields.Boolean(
         string='Pago aprobado'
     ) """
-    fecha_arreglo = fields.Date(
-        string='Fecha programada de pago'
-    )
-    validado = fields.Boolean(
-        string='Validado Hacienda'
-    )
-    
+    fecha_arreglo = fields.Date(string="Fecha programada de pago")
+    validado = fields.Boolean(string="Validado Hacienda")
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    instructor = fields.Boolean(
-        string='Instructor'
-    )
+    instructor = fields.Boolean(string="Instructor")
 
     """ IBAN = fields.Char(
         string='Cuenta IBAN'
     ) """
 
-    gobierno = fields.Boolean(
-        string='¿Gobierno?'
-    )
+    gobierno = fields.Boolean(string="¿Gobierno?")
+
+    is_factura_recordatorio = fields.Boolean(string='No enviar recordatorio', default=False, help='No enviar recordatorio cuando se encuentre la factura pendiente')
--- has_string_cr/models/res_partner.py
+++ has_string_cr/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 
 
 class res_partner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
